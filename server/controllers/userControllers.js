5a4f0868eb6d6135087067f6ff424c89eeed6e33
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-12 15:10:50 +0200

    fix the error

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -5,7 +5,7 @@ const generateToken = require("../config/generateToken");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
 
-  if (name || email || password) {
+  if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please Enter all the Fields");
   }
