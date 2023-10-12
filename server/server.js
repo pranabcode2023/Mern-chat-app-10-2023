c077629650990960ebbaec02f063bc5f24339404
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-12 13:32:57 +0200

    import userRoutes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectMongoDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
@@ -12,15 +13,18 @@ app.get("/", (req, res) => {
   res.send("API is Running Successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+//NOTE - test purpose
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
 
-app.get("/api/chat/:id", (req, res) => {
-  //   console.log(req.params.id);
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+// app.get("/api/chat/:id", (req, res) => {
+//   //   console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server started on port ${PORT}`.bgBlue.bold));
