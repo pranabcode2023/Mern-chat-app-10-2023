2327e047e9ea048962e42d04f3a98ff4d6f0731c
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-12 10:57:39 +0200

    function added to connect mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const connectMongoDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected:${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error:${error.message}`);
+    process.exit();
+  }
+};
+
+module.exports = connectMongoDB;
