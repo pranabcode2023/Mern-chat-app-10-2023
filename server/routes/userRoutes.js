7a2122e6be3064dd428c73cf4e0a2e69e3fd7d2f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-13 12:55:47 +0200

    note added for endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { registerUser } = require("../controllers/userControllers");
 
 const router = express.Router();
-
+//NOTE - in this way you can put multiple endpoint
 router.route("/").post(registerUser);
 // router.post('/login', authUser)
 
