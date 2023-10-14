0671ef029f86d54e180558ec7eafd39dd5589f89
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-14 11:53:15 +0200

    route added for all users with protect middleware to for authorization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 //NOTE - in this way you can put multiple endpoint
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, allUsers);
 router.post("/login", authUser);
+
+// router.route('/').get(allUsers)
 
 module.exports = router;
