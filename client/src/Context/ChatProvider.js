55214efa418bdfe9058de36ed633137322695497
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-14 15:07:40 +0200

    ChatProvider context added

---
+++ client/src/Context/ChatProvider.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  //NOTE - this useState i can use for whole app bcoz it pass ass value inside chatcontext provider
+  const [user, setUser] = useState();
+  const history = useHistory();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) {
+      history.push("/");
+    }
+  }, [history]);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
