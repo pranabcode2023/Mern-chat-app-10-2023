0e6448c52730f080d27cae97f9623288b7343b87
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-17 12:05:20 +0200

    function for side menu and others components added from chakra UI

--- client/src/components/Chat/SideMenu.js
+++ client/src/components/Chat/SideMenu.js
@@ -1,7 +1,160 @@
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Input,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const SideMenu = () => {
-  return <div>SideMenu</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+  const history = useHistory();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const logotHandler = () => {
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  };
+  const toast = useToast();
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: "Please Enter something in search",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "top-left",
+      });
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Something went wrong ",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+  return (
+    <>
+      <Flex
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant={"ghost"} onClick={onOpen}>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <Text d={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+
+        <Text color={"black"} fontSize={"2xl"} fontFamily={"work sans"}>
+          Chat-App
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <i class="fa-solid fa-bell"></i>
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+
+          <Menu>
+            <MenuButton
+              as={Button}
+              rightIcon={<i class="fa-solid fa-chevron-down"></i>}
+            >
+              {/* //NOTE - Avatar for round prifile picture  */}
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem color={"black"}>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem color={"black"} onClick={logotHandler}>
+                Log Out
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Flex>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBlockEndWidth={"1px"}>Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display={"flex"} pb={2}>
+              <Input
+                placeholder="Search by Name or E-mail"
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default SideMenu;
