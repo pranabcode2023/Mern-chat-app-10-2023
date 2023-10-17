630eaaa912a15c1a91cbdf85b9a9aa478cf1d9eb
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-17 16:42:14 +0200

    in ChatPage  displaying sideMenu, MyChats, ChatBox

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -3,23 +3,27 @@ import SideMenu from "../components/Chat/SideMenu";
 import MyChats from "../components/Chat/MyChats";
 import ChatBox from "../components/Chat/ChatBox";
 import { Box, Flex } from "@chakra-ui/react";
+import { useState } from "react";
 
 const ChatPage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
   const { user } = ChatState();
 
   return (
     <div style={{ width: "100%" }}>
       {user && <SideMenu />}
-      <Flex
-        display={"flex"}
+      <Box
+        display="flex"
         justifyContent="space-between"
         w="100%"
         h="91.5vh"
         p="10px"
       >
-        <Box>{user && <MyChats />}</Box>
-        <Box>{user && <ChatBox />}</Box>
-      </Flex>
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
     </div>
   );
 };
