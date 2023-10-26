0192f5c9d7bcfc09dd9a607e4b9ef37fac5282c9
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-26 14:48:31 +0200

    edited

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
+const { sendMessage } = require("../controllers/messageControllers");
 
-// router.route("/").post(protect, snedMessage );
+router.route("/").post(protect, sendMessage);
 // router.route("/:chatId").get(protect, allMessages );
 
 module.exports = router;
