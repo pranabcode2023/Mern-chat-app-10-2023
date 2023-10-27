58ffb75a303d0da9c36e638d04d24cac26b0154c
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-27 10:16:36 +0200

    function added to send send message

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -1,6 +1,38 @@
-const expressAsyncHandler = require("express-async-handler");
 const asyncHandler = require("express-async-handler");
+const Message = require("../models/messageModel");
+const User = require("../models/userModel");
+const Chat = require("../models/chatModel");
 
-const sendMessage = expressAsyncHandler(async (req, res) => {});
+const sendMessage = asyncHandler(async (req, res) => {
+  const { content, chatId } = req.body;
+  if (!content || !chatId) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+  let newMessage = {
+    sender: req.user._id,
+    content: content,
+    chat: chatId,
+  };
+
+  try {
+    let message = await Message.create(newMessage);
+    message = await message.populate("sender", " name pic").execpopulate();
+    message = await message.populate("chat").execpopulate();
+    message = await User.populate(message, {
+      path: "chat.users",
+      select: "name pic email",
+    });
+
+    await Chat.findByIdAndUpdate(req.body.chatId, {
+      latestMessage: message,
+    });
+
+    res.json(message);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 
 module.exports = { sendMessage };
