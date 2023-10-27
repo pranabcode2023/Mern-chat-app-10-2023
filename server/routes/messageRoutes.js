40f966a2c1bcd0a8368fa25d440d553548c9c7a6
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-10-27 10:51:02 +0200

    added routes for all messages for  chat

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
-const { sendMessage } = require("../controllers/messageControllers");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers");
 
 router.route("/").post(protect, sendMessage);
-// router.route("/:chatId").get(protect, allMessages );
+router.route("/:chatId").get(protect, allMessages);
 
 module.exports = router;
