e4b44dabd87688e7e1a65f4a414593ce495e9c71
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-01 19:02:23 +0100

    edited

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "development ") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../client/build")));
 
   app.get("*", (req, res) =>
