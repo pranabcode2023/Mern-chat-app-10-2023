db2250bddd62e7aa8beb2c66187b2a1eb184088f
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-09 20:13:56 +0100

    only render url

--- server/server.js
+++ server/server.js
@@ -91,7 +91,8 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 70000,
   cors: {
-    origin: ["http://localhost:3000", "https://chat-app-rfe4.onrender.com"],
+    origin: ["https://chat-app-rfe4.onrender.com"],
+    // origin: ["http://localhost:3000"],
   },
 });
 
