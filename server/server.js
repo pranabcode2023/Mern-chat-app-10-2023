0063d9b45075e0e9b322c8f2b390bf21df4069db
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-09 13:56:42 +0100

    added vercel deployment function

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 
 dotenv.config();
 //NOTE - call function to connect MongoDB
@@ -29,14 +29,14 @@ app.use("/api/message", messageRoutes);
 
 //NOTE - ----------------vercel deployment ---------------------
 
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://mern-chat-app-10-2023-client.vercel.app",
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://mern-chat-app-10-2023-client.vercel.app",
 
-//   //NOTE - url put into env file
-//   // process.env.LOCALHOST_CLIENT,
-//   // process.env.VERCEL_CLIENT,
-// ];
+  //NOTE - url put into env file
+  // process.env.LOCALHOST_CLIENT,
+  // process.env.VERCEL_CLIENT,
+];
 
 // const corsOptions = {
 //   origin: function (origin, callback) {
@@ -48,24 +48,24 @@ app.use("/api/message", messageRoutes);
 //   },
 // };
 
-// app.use(cors());
+app.use(cors());
 // app.use(cors(corsOptions));
 
 // --------------------------for render deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "../client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../client/build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is Running Successfully");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "../client/build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is Running Successfully");
+//   });
+// }
 
 // --------------------------deployment------------------------------
 
