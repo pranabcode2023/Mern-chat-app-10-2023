21503d5a0e18db1cc02b28deea02d96abec1ef3d
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-09 20:09:20 +0100

    render url added

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 70000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://chat-app-rfe4.onrender.com"],
   },
 });
 
