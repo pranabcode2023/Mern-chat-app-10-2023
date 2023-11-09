4e815bf3f81b54253811ab3520a91d31bf408cc0
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-09 14:00:23 +0100

    corsOptions added

--- server/server.js
+++ server/server.js
@@ -38,15 +38,15 @@ const allowedOrigins = [
   // process.env.VERCEL_CLIENT,
 ];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
 app.use(cors());
 // app.use(cors(corsOptions));
